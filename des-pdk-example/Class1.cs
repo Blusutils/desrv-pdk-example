2a5cdb6f0e97e99e67479b7d703b7e086ace51ff
Egor Bron <71507444+EgorBron@users.noreply.github.com>
2022-12-01 19:22:42 +0300

    update template

--- des-pdk-example/Class1.cs
+++ des-pdk-example/Class1.cs
@@ -1,45 +1,41 @@
-﻿using DESPDK;
-
+﻿using DESrv.PDK;
 namespace DESPDKExamples1 {
     [PDKExtension]
-    public class ExamplePlugin : PDKAbstractExtension {
-        // ID of extension. It mustn't contain spaces and special symbols (for example dots)
-        public new string ID = "ExamplePlugin";
-        // Type of extension: 1 - plugin, 2 - addon
-        public new int ExtType = 1;
-        // Readable name of extension
-        public new string Name = "Example Plugin";
-        // Description for extension
-        public new string Description = "Just an example";
-        // Version of extension
-        public new string Version = "1.0.0";
-        // Version of DESrv what this extension supports
-        public new string DESVersion = "1.0.0";
-        // Author of extension
-        public new string Author = "Blusutils";
-        // Array of dependencies for extension
-        //public new string[] Dependencies = Array.Empty<string>();
-        // ID of extension to which this extension refers (for addons)
-        //public new string Reference = "";
-
+    public class ExamplePlugin : AbstractPDKExtension {
+        // Metadata of extension
+        public override ExtensionMetadata Metadata { get; set; } = new ExtensionMetadata() {
+            // ID of extension. It mustn't contain spaces and special symbols (for example dots)
+            ID = "ExamplePlugin",
+            // Type of extension: 1 - plugin, 2 - addon, 3 - random provider
+            ExtType = 1,
+            // Readable name of extension
+            Name = "Example Plugin",
+            // Description for extension
+            Description = "Just an example",
+            // Author of extension
+            Author = "Blusutils",
+            // Version of extension
+            Version = "0.1.0",
+            // Version of DESrv what this extension supports
+            DESVersion= "1.3.0",
+            // Array of dependencies for extension
+            //Dependencies = new string[] {},
+            // ID of extension to which this extension refers (for addons)
+            //Reference = ""
+        };
         // Main method
         public override void Entrypoint() {
             Console.WriteLine("Hello DESrv!");
         }
-
-        // Load addon to this extension (for plugins)
-        public override void LoadSubExtension(PDKAbstractExtension extension) {
-            //extension.Load();
-        }
-
         // Event what calls when extension loads
         public override void Load() {
 
         }
-
         // Event what calls when extension unloads
         public override void Unload() {
 
         }
+        // Load addon to this extension (for plugins)
+        public override void LoadSubExtension(AbstractPDKExtension extension) => extension.Load();
     }
 }
